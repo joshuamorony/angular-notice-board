5f9eeb378523d7e224c26442fbd1cae59f7c3df3
Joshua Morony <joshua.morony@gmail.com>
2022-11-22 09:30:16 +1030

    initial commit

--- src/app/layout/ui/nav-bar.component.ts
+++ src/app/layout/ui/nav-bar.component.ts
@@ -8,33 +8,18 @@ import { RouterLink } from "@angular/router";
     <nav>
       <div class="container">
         <div class="items">
-          <div class="flex flex-shrink-0 items-center">
+          <div class="logo">
             <img
-              class="block h-8 w-auto lg:hidden"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="Your Company"
-            />
-            <img
-              class="hidden h-8 w-auto lg:block"
+              class="block h-8 w-auto"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
               alt="Your Company"
             />
           </div>
-          <div class="hidden sm:ml-6 sm:block">
+          <div class="links">
             <div class="flex space-x-4">
-              <a
-                routerLink="/"
-                class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Dashboard
-              </a>
+              <a routerLink="/" class="active">Dashboard</a>
 
-              <a
-                routerLink="/create"
-                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Create
-              </a>
+              <a routerLink="/create" class="">Create</a>
             </div>
           </div>
         </div>
@@ -57,6 +42,22 @@ import { RouterLink } from "@angular/router";
 
       .items {
         @apply flex flex-1 items-center justify-center sm:items-stretch sm:justify-start;
+      }
+
+      .logo {
+        @apply flex flex-shrink-0 items-center;
+      }
+
+      .links {
+        @apply hidden sm:ml-6 sm:block;
+
+        a {
+          @apply text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium;
+        }
+
+        .active {
+          @apply bg-gray-900 hover:bg-gray-900 text-white;
+        }
       }
     `,
   ],
